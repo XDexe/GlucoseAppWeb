bd9d696d9e6502b56ba2fdef5ce73a22fd86d6ac
Lorenzini <lamso.0ff@gmail.com>
2025-03-11 14:37:43 +0100

    authentification sans cryptage

--- src/main/java/fr/lamsoent/glucoseapplication/controller/PersonneController.java
+++ src/main/java/fr/lamsoent/glucoseapplication/controller/PersonneController.java
@@ -8,6 +8,7 @@ import jakarta.ejb.EJB;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.eclipse.persistence.oxm.annotations.XmlNamedAttributeNode;
 
@@ -20,39 +21,42 @@ import java.util.List;
 public class PersonneController implements Serializable {
     private List<Personne> listPersonne = new ArrayList<Personne>();
     private Personne personneLogin=new Personne();
+
+    @Inject
+    private UtilisateurController utilisateurController;
+
     @EJB
     private PersonneModel personneModel;
 
     public String AuthPersonne() {
         System.out.println("Authentification en cours");
         listPersonne=personneModel.read();
-        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 
         boolean isLogged = false;
 
         for (Personne p : listPersonne) {
+            if (p.getIdentifiant() ==null || p.getIdentifiant().isEmpty()|| p.getMotDePasse()==null || p.getMotDePasse().isEmpty()){
+                System.out.println("id personne " + p.getIdPersonne());
+                continue;
+            }
             if (p.getIdentifiant().equals(personneLogin.getIdentifiant()) && p.getMotDePasse().equals(personneLogin.getMotDePasse())) {
                 personneLogin=p;
                 isLogged = true;
                 break;
             }
-
+        }
 
         if(isLogged){
             System.out.println("Authentification Réussi");
-            try {
-                if(personneLogin.getClass().equals(Utilisateur.class)){
-                    return "listeActivites";
-                }
-                externalContext.redirect("Accueil.xhtml");
-            } catch (Exception e) {
-                e.getMessage();
+            if(personneLogin.getClass().equals(Utilisateur.class)){
+                Utilisateur utilisateur=(Utilisateur) personneLogin;
+                utilisateurController.setUtilisateur(utilisateur);
+                System.out.println(utilisateurController.getUtilisateur().getIdPersonne());
+                    return "listeActivites?faces-redirect=true";
             }
         }else {
             System.out.println("Authentification échoué");
         }
-
-    }
         return "";
 }
 
